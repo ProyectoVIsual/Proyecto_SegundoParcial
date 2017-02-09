a813bd148d84cd42647112bbf0bce40d6cbf289a
majocabr <majocabr.93@gmail.com>
2017-02-09 03:16:01 -0500

    Update voto2016.sql

--- voto2016.sql
+++ voto2016.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-12-2016 a las 05:56:06
+-- Tiempo de generación: 09-02-2017 a las 09:14:17
 -- Versión del servidor: 10.1.9-MariaDB
 -- Versión de PHP: 5.6.15
 
@@ -63,16 +63,39 @@ CREATE TABLE IF NOT EXISTS `candidato` (
   `dignidad` varchar(50) NOT NULL,
   `apellido` varchar(25) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `candidato`
 --
 
 INSERT INTO `candidato` (`id`, `nombre`, `user`, `pass`, `lista`, `Votos`, `cedula`, `edad`, `dignidad`, `apellido`) VALUES
-(1, 'fernando', 'fernando', 'fernando123', 'Lista 1', 0, 1220356846, 30, 'presidente', 'ordoñez'),
-(2, 'carlos', 'carlos', 'carlos123', 'Lista 2', 0, 356589356, 35, 'presidente', 'noboa'),
-(3, 'paco', 'pacola20', '123456', '20', 0, 1568854963, 55, 'presidente', 'moncayo');
+(1, 'fernando', 'fernando', 'fernando123', '1', 0, 1220356846, 30, 'presidente', 'ordoñez'),
+(2, 'carlos', 'carlos', 'carlos123', '2', 1, 356589356, 35, 'presidente', 'noboa'),
+(3, 'paco', 'pacola20', '123456', '20', 0, 1568854963, 55, 'presidente', 'moncayo'),
+(4, 'Edgar ', 'edgja10', 'edgja10', '10', 0, 536356984, 46, 'Parlamento Andino', 'Jara '),
+(5, 'Monavel ', 'monma1', 'monma1', '1', 0, 1254564786, 32, 'Parlamento Andino', 'Macias'),
+(6, 'Lourdes', 'louti12', 'louti12', '12', 0, 1356535604, 55, 'Parlamento Andino', 'Tiban'),
+(7, 'Yaritza', 'yarive1', 'yarive1', '1', 0, 1265358653, 45, 'Parlamento Andino', 'Vera'),
+(8, 'jose', 'joserr35', 'joserr35', '35', 0, 1325645565, 48, 'Asambleista', 'Serrano'),
+(9, 'Luis', 'luquis35', 'luquis35', '35', 0, 156356689, 36, 'Asambleista', 'Quishpi'),
+(10, 'Pubenza', 'pubfuen3', 'pubfuen3', '3', 0, 1325356894, 50, 'Parlamento Andino', 'Fuentes'),
+(11, 'Roxana ', 'roxpig6', 'roxpig6', '6', 0, 1356195362, 29, 'Parlamento Andino', 'Piguave '),
+(12, 'Wilson ', 'wilsa7', 'wilsa7', '7', 0, 254356456, 48, 'Parlamento Andino', 'Sanchez'),
+(13, 'nulo', 'vnulo0', 'vnulo0', '0', 0, 0, 0, 'presidente', ' '),
+(14, 'blanco', 'vblanc0', 'vblanc0', '0', 0, 0, 0, 'presidente', ' '),
+(15, 'Viviana', 'vivibo35', 'vivibo35', '35', 0, 995632359, 30, 'Asambleista', 'Bonilla'),
+(16, 'MOISES', 'moitac6', 'moitac6', '6', 0, 653456823, 48, 'Asambleista', 'TACLE'),
+(17, 'PAOLA', 'paovin6', 'paovin6', '6', 0, 956234586, 27, 'Asambleista', 'VINTIMILLA'),
+(18, 'Guillermo', 'guisan21', 'guisan21', '21', 0, 653569520, 38, 'Asambleista', 'Santos'),
+(19, 'Mae', 'maemon21', 'maemon21', '21', 0, 1826552380, 49, 'Asambleista', 'Montaño'),
+(20, 'María José', 'majoce5', 'majoce5', '5', 0, 633526405, 28, 'Asambleista', 'Cedeno '),
+(21, 'Alex ', 'alalma5', 'alalma5', '5', 0, 56348689, 36, 'Asambleista', 'Almache'),
+(22, 'Priscilla', 'prisna17', 'prisna17', '17', 0, 953456850, 31, 'Asambleista', 'Naranjo'),
+(23, 'blanco', 'vpablanc0', 'vpablanc0', '0', 0, 1, 0, 'Parlamento Andino', ' '),
+(24, 'nulo', 'nulpa0', 'nulpa0', '0', 0, 1, 0, 'Parlamento Andino', ' '),
+(25, 'nulo', 'nulas0', 'nulas0', '0', 0, 2, 0, 'Asambleista', '  '),
+(26, 'blanco', 'blasn0', 'blasn0', '0', 0, 2, 0, 'Asambleista', ' ');
 
 -- --------------------------------------------------------
 
@@ -108,17 +131,22 @@ DROP TABLE IF EXISTS `votante`;
 CREATE TABLE IF NOT EXISTS `votante` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cedula` int(11) NOT NULL,
+  `voto` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `votante`
 --
 
-INSERT INTO `votante` (`id`, `cedula`) VALUES
-(1, 123456789),
-(2, 1234567809),
-(3, 123456788);
+INSERT INTO `votante` (`id`, `cedula`, `voto`) VALUES
+(1, 123456789, 0),
+(2, 1234567809, 0),
+(3, 123456788, 0),
+(4, 987654321, 0),
+(5, 12345678, 0),
+(6, 99887766, 0),
+(7, 55443322, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
